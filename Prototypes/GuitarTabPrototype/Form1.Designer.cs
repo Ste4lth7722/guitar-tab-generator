9b26407d2f9f2c2c19ef4447e635ed78fa381808
Sam Hunt <sam@poggle.org>
2025-10-16 12:21:50 +0100

    Adding everything so far
    can now record audio and (hopefully) play audio from a file.

--- Prototypes/GuitarTabPrototype/Form1.Designer.cs
+++ Prototypes/GuitarTabPrototype/Form1.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace GuitarTabPrototype
+﻿ namespace GuitarTabPrototype
 {
     partial class Form1
     {
@@ -28,12 +28,102 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            startPlaybackButton = new Button();
+            endPlaybackButton = new Button();
+            beginRecordingButton = new Button();
+            fileNameTextbox = new TextBox();
+            label1 = new Label();
+            stopRecordingButton = new Button();
+            SuspendLayout();
+            // 
+            // startPlaybackButton
+            // 
+            startPlaybackButton.Location = new Point(552, 51);
+            startPlaybackButton.Margin = new Padding(4, 2, 4, 2);
+            startPlaybackButton.Name = "startPlaybackButton";
+            startPlaybackButton.Size = new Size(193, 47);
+            startPlaybackButton.TabIndex = 0;
+            startPlaybackButton.Text = "Start Playback";
+            startPlaybackButton.UseVisualStyleBackColor = true;
+            startPlaybackButton.Click += startPlaybackButton_Click;
+            // 
+            // endPlaybackButton
+            // 
+            endPlaybackButton.Location = new Point(552, 132);
+            endPlaybackButton.Margin = new Padding(4, 2, 4, 2);
+            endPlaybackButton.Name = "endPlaybackButton";
+            endPlaybackButton.Size = new Size(193, 47);
+            endPlaybackButton.TabIndex = 0;
+            endPlaybackButton.Text = "End Playback";
+            endPlaybackButton.UseVisualStyleBackColor = true;
+            endPlaybackButton.Click += endPlaybackButton_Click;
+            // 
+            // beginRecordingButton
+            // 
+            beginRecordingButton.Location = new Point(28, 51);
+            beginRecordingButton.Margin = new Padding(4, 2, 4, 2);
+            beginRecordingButton.Name = "beginRecordingButton";
+            beginRecordingButton.Size = new Size(217, 70);
+            beginRecordingButton.TabIndex = 1;
+            beginRecordingButton.Text = "Begin Recording";
+            beginRecordingButton.UseVisualStyleBackColor = true;
+            beginRecordingButton.Click += beginRecordingButton_Click;
+            // 
+            // fileNameTextbox
+            // 
+            fileNameTextbox.Location = new Point(28, 183);
+            fileNameTextbox.Margin = new Padding(4, 2, 4, 2);
+            fileNameTextbox.Name = "fileNameTextbox";
+            fileNameTextbox.Size = new Size(201, 39);
+            fileNameTextbox.TabIndex = 2;
+            fileNameTextbox.TextChanged += fileNameTextbox_TextChanged;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(28, 149);
+            label1.Margin = new Padding(4, 0, 4, 0);
+            label1.Name = "label1";
+            label1.Size = new Size(194, 32);
+            label1.TabIndex = 3;
+            label1.Text = "Enter File Name..";
+            // 
+            // stopRecordingButton
+            // 
+            stopRecordingButton.Location = new Point(254, 51);
+            stopRecordingButton.Margin = new Padding(6, 6, 6, 6);
+            stopRecordingButton.Name = "stopRecordingButton";
+            stopRecordingButton.Size = new Size(208, 70);
+            stopRecordingButton.TabIndex = 4;
+            stopRecordingButton.Text = "Stop Recording";
+            stopRecordingButton.UseVisualStyleBackColor = true;
+            stopRecordingButton.Click += stopRecordingButton_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(13F, 32F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(stopRecordingButton);
+            Controls.Add(label1);
+            Controls.Add(fileNameTextbox);
+            Controls.Add(beginRecordingButton);
+            Controls.Add(endPlaybackButton);
+            Controls.Add(startPlaybackButton);
+            Margin = new Padding(4, 2, 4, 2);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Button startPlaybackButton;
+        private Button endPlaybackButton;
+        private Button beginRecordingButton;
+        private TextBox fileNameTextbox;
+        private Label label1;
+        private Button stopRecordingButton;
     }
 }
--- Prototypes/GuitarTabPrototype/Form1.cs
+++ Prototypes/GuitarTabPrototype/Form1.cs
@@ -1,10 +1,77 @@
+using Microsoft.VisualBasic.ApplicationServices;
+using Microsoft.VisualBasic.Devices;
+using NAudio.Wave;
+using static System.Net.Mime.MediaTypeNames;
+using System.Security.Policy;
+using System.Diagnostics;
+
 namespace GuitarTabPrototype
 {
     public partial class Form1 : Form
     {
+        private WaveOutEvent outputDevice;
+        private AudioFileReader audioFile;
+
+        private String fileName = "";
+
+        Recording recordingManager;
+
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void startPlaybackButton_Click(object sender, EventArgs e)
+        {
+            if (outputDevice == null)
+            {
+                outputDevice = new WaveOutEvent();
+                outputDevice.PlaybackStopped += OnPlaybackStopped;
+            }
+            if (audioFile == null)
+            {
+                audioFile = new AudioFileReader(@"C:\Users\SH166361\OneDrive - Hills Road Sixth Form College\Computer Science\NEA\Prototypes\GuitarTabPrototype\Files\Audio\test1.mp3");
+                outputDevice.Init(audioFile);
+            }
+            outputDevice.Play();
+        }
+
+        private void endPlaybackButton_Click(object sender, EventArgs e)
+        {
+            if (outputDevice != null)
+            {
+                outputDevice.Stop();
+            }
+        }
+
+        private void OnPlaybackStopped(object sender, StoppedEventArgs args)
+        {
+
+        }
+
+        private void beginRecordingButton_Click(object sender, EventArgs e)
+        {
+            if (recordingManager == null)
+            {
+                recordingManager = new Recording();
+            }
+            //var outputFolder = @"C:\Users\samue\OneDrive - Hills Road Sixth Form College\Computer Science\NEA\Prototypes\GuitarTabPrototype\Files\Audio\" + fileNameTextbox.Text + ".mp3";
+            var outputFolder = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), fileNameTextbox.Text) + ".mp3";
+            recordingManager.beginRecording(outputFolder);
+        }
+
+        private void fileNameTextbox_TextChanged(object sender, EventArgs e)
+        {
+            Debug.WriteLine(fileNameTextbox.Text);
+        }
+
+        private void stopRecordingButton_Click(object sender, EventArgs e)
+        {
+                if (recordingManager != null)
+            {
+                recordingManager.endRecording();
+            }
         }
     }
 }
